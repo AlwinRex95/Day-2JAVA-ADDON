7e3cc1520e61cc0e99db5543e4b7641b8cc5774d
Alwin Rex A <alwinrex95@gmail.com>
2025-06-06 14:22:46 +0530

    Add files via upload

---
+++ NumberPatterns.java
@@ -0,0 +1,23 @@
+package Day2;
+
+public class NumberPatterns {
+
+	public static void main(String[] args) {
+		
+		int n=5;
+		for (int i = 1; i <=n; i++) {
+			//i=1; 1<=5 true
+			//i++= 1++ 2
+			//i=2 2<=5 true
+			for (int j = 1; j <=i; j++) {
+				
+				System.out.print(i);
+				
+			}
+				System.out.println(" ");
+		}
+		
+		
+	}
+
+}
